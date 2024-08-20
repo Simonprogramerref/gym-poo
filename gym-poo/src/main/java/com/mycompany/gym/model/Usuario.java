dea3bd50070dd76b10630bbfc52b30b76a3f3056
Sayposada <siposadaa@unal.edu.co>
2024-08-19 22:41:40 -0500

    Main intentando hablar con el usuario

--- gym-poo/src/main/java/com/mycompany/gym/model/Usuario.java
+++ gym-poo/src/main/java/com/mycompany/gym/model/Usuario.java
@@ -8,6 +8,8 @@ public class Usuario {
     private int edad;
     private int disponibilidad;
     private List<String> preferencias;
+    
+    public Usuario(){ }
 
     public Usuario(String nombre, int edad, int disponibilidad, List<String> preferencias) {
         this.nombre = nombre;
@@ -16,6 +18,7 @@ public class Usuario {
         this.preferencias = preferencias;
     }
 
+    //getters
     public String getNombre() {
         return nombre;
     }
@@ -31,6 +34,27 @@ public class Usuario {
     public List<String> getPreferencias() {
         return preferencias;
     }
+    
+    //Setters
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDisponibilidad(int disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public void setPreferencias(List<String> preferencias) {
+        this.preferencias = preferencias;
+    }
+    
+    
+    
 
     public void asignarCoach(Coach coach) {
         if (this.preferencias.contains(coach.getEspecialidad().name())) {
--- gym-poo/src/main/java/com/mycompany/gym/poo/Main.java
+++ gym-poo/src/main/java/com/mycompany/gym/poo/Main.java
@@ -9,16 +9,51 @@ import com.mycompany.gym.service.Gimnasio;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Gimnasio gimnasio = new Gimnasio();
 
-        // Crear usuario
+        //crear usuario por consola 
+        Scanner scanner = new Scanner(System.in);
+        Usuario user1 = new Usuario();
+
+        System.out.println("Cuál es tu nombre???");
+        user1.setNombre(scanner.nextLine());
+
+        System.out.println("Gracias: " + user1.getNombre() + "\nAhora, te pediré información adicional:" + "\n----->");
+
+        System.out.println("¿Cuál es tu edad?");
+        int edad = scanner.nextInt();
+        if (edad < 0 || edad < 100) {
+            System.out.println("Valor incorrecto, debe ser un entero, haga las cosas bien.");
+        } else {
+
+            user1.setEdad(scanner.nextInt());
+
+        }
+        scanner.nextLine();
+
+        System.out.println("¿Cuál es tu disponibilidad horaria? Refiríendome a las horas disponibles para entrenar");
+        int disponibilidad = scanner.nextInt();
+        if (disponibilidad < 0 || disponibilidad > 168) {
+            System.out.println("Nos halaga saber que estás así de motivado, pero el día tiene 24 horas y la semana 168. Escoge un valor realista.");
+        } else {
+            user1.setDisponibilidad(disponibilidad);
+        }
+        scanner.nextLine();
+
         List<String> preferencias = new ArrayList<>();
+
         preferencias.add("CARDIO");
         preferencias.add("YOGA");
+        preferencias.add("FUERZA");
+        preferencias.add("ZUMBA");
+
+        // Crear usuario
+        // Crear usuario
         Usuario usuario = new Usuario("Juan", 30, 8, preferencias);
 
         // Registrar usuario
